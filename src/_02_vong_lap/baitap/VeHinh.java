5f2d89aec6827d2565ebadadce7cc265fe8896b5
bao <baoonlinemmo@gmail.com>
2021-06-04 10:24:22 +0700

    update

--- src/_02_vong_lap/baitap/VeHinh.java
+++ src/_02_vong_lap/baitap/VeHinh.java
@@ -9,18 +9,31 @@ public class VeHinh {
             System.out.println("");
         }
         System.out.println("");
-        for (int x = 0;x<7;x++){        //bai2
-            for (int y =0;y<=x;y++){
+        for (int x = 0;x<=5;x++){        //bai2
+            for (int y =0;y<x;y++){
                 System.out.print("*");
             }
             System.out.println("");
         }
         System.out.println("");         // bai3
         for (int a = 7;a>=1;a--){
-            for (int b = 0;b<a;b++){
+            for (int b = 1;b<=a;b++){
                 System.out.print("*");
             }
             System.out.println("");
+        }
+        System.out.println("");
+        int m = 5, n = 5;
+        for (int i = 1; i <= 5; i++){
+            for (int j = 1; j <= 2 * 5 - 1; j++){
+                if (j >= m && j <= n)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            m--;
+            n++;
+            System.out.print("\n");
         }
     }
 }
