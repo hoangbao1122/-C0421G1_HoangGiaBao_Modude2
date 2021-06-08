c33b6909b5f952054e227fb7c73d3582dd1d8f22
bao <baoonlinemmo@gmail.com>
2021-06-08 16:43:41 +0700

    update

--- src/_04_lop_doi_tuong/baitap/fan/Fan.java
+++ src/_04_lop_doi_tuong/baitap/fan/Fan.java
@@ -4,20 +4,33 @@ public class Fan {
     int SLOW = 1;
     final int MEDIUM = 2;
     final int FAST = 3;
-
+    private int speed = SLOW;
     private boolean on = true;
-
+    private int koHoatDong = 4;
     private double radius = 5;
-
 
 
     private String color = "blue";
 
     public int speed() {
-        return this.SLOW;
+        return this.speed;
     }
-    public int setSpeed( int newSpeed){
-        return this.SLOW = newSpeed;
+    public void setSpeed(int speed){
+        this.speed = speed;
+    }
+
+    public String displaySpeed(){
+        if (this.speed==SLOW){
+            return "slow";
+        }else if(this.speed == MEDIUM){
+            return "medium";
+        }else if(this.speed == FAST){
+            return "fast";
+        }else if (this.speed == koHoatDong){
+            return "ko chạy";
+        }else {
+            return "lỗi";
+        }
     }
 
     public boolean getOn() {
@@ -25,42 +38,51 @@ public class Fan {
     }
 
     public boolean getOff() {
-       return this.on = false;
+        this.on = false;
+        this.setSpeed(4);
+        return false;
     }
 
-    public double getRadius() {
+    public double radius() {
         return this.radius;
     }
-    public void setRadius(double newRadius){
 
-         this.radius = newRadius;
+    public void setRadius(double newRadius) {
+
+        this.radius = newRadius;
     }
+
     public String getColor() {
         return this.color;
     }
+
     public void setColor(String newColor) {
         this.color = newColor;
     }
+    public String thongBao(){
+        String alert;
+        if (this.getOn()){
+           return alert =  "fan is on";
+        }else{
+           return alert =  "fan is off";
+        }
+    }
 
     @Override
-    public String toString() {  // on
+    public String toString() {
+
         return "Fan{" +
                 "on=" + on +
-                ",Speed=" + SLOW +
+                ",Speed=" + this.displaySpeed() +
                 ", radius=" + radius +
 
                 ", color='" + color +
                 ", fan is on" + '\'' +
+                ", trạng thái " + this.thongBao() +
                 '}';
+
     }
 
-    public String toStringFalse() {  // off
-        return "Fan{" +
-                "on=" + getOff() +
-                ", radius=" + radius +
-                ", color='" + color + '\'' +
-                ", fan is off" +
-                '}';
-    }
 
+  
 }
--- src/_04_lop_doi_tuong/baitap/fan/Main.java
+++ src/_04_lop_doi_tuong/baitap/fan/Main.java
@@ -3,11 +3,13 @@ package _04_lop_doi_tuong.baitap.fan;
 public class Main {
     public static void main(String[] args) {
         Fan fan1 = new Fan();
-        fan1.setSpeed(10);
+        fan1.getOn();
         fan1.setRadius(10);
-        fan1.setColor("yellow");
-        System.out.println("bật " + fan1.toString());
+        fan1.setSpeed(2);
+        System.out.println(fan1.toString());
+
         Fan fan2 = new Fan();
-        System.out.println("tắt " + fan2.toStringFalse());
+        fan2.getOff();
+        System.out.println(fan2.toString());
     }
 }
