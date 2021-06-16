dd47d1aca46dfdb34507ab7e4bf39014c130c354
bao <baoonlinemmo@gmail.com>
2021-06-16 22:33:06 +0700

    update

--- src/_08_clear_code/baitap/TennisGame.java
+++ src/_08_clear_code/baitap/TennisGame.java
@@ -2,13 +2,11 @@ package _08_clear_code.baitap;
 
 public class TennisGame {
 
-    public static String getScore(String player1Name, String player2Name, int player1, int player2) {
+    public static void getScore(String player1Name, String player2Name, int player1Score, int player2Score) {
         String score = "";
-        int initialPoint= 0;
-        if (player1==player2)
-        {
-            switch (player1)
-            {
+        int initialPoint = 0;
+        if (player1Score == player2Score) {
+            switch (player1Score) {
                 case 0:
                     score = "Love-All";
                     break;
@@ -28,42 +26,62 @@ public class TennisGame {
 
             }
         }
-        else if (player1>=4 || player2>=4)
+
+    }
+    public static String getScoreWhenIsByPoint(int player1Score) {
+        String score;
+        switch (player1Score)
         {
-            int minusResult = player1-player2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1){
-                    initialPoint = player1;
-                }
-                else {
-                    score += "-";
-                    initialPoint = player2;
-                }
-                switch(initialPoint)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+            case 0:
+                score = "Love-All";
+                break;
+            case 1:
+                score = "Fifteen-All";
+                break;
+            case 2:
+                score = "Thirty-All";
+                break;
+            case 3:
+                score = "Forty-All";
+                break;
+            default:
+                score = "Deuce";
+                break;
+
         }
         return score;
     }
+    public static int getMinusResult(int player1Score, int player2Score){
+        return player1Score - player2Score;
+    }
+    public static void getScoreWhenMoreThan4(int player1Score, int player2Score) {
+        String score;
+        int minusResult = getMinusResult(player1Score, player2Score);
+//        if (minusResult==1) score ="Advantage player1";
+//        else if (minusResult ==-1) {
+//            score ="Advantage player2";
+//        }
+//        else if (minusResult>=2) {
+//            score = "Win for player1";
+//        }
+//        else {
+//            score ="Win for player2";
+//        }
+//        return score;
+        if (minusResult >=2){
+            score = "Win for player 1";
+        }
+        switch (minusResult){
+            case 1:{
+                score =  "Advantage player2";
+            }
+            case 2:{
+                score = "Advantage player2";
+            }
+            default:{
+                score ="Win for player2";
+            }
+        }
+    }
+
 }
